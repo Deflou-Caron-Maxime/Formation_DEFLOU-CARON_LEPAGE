f1a6fe1190e6a0e222b2296681f172978b23e07f
Maxime Deflou-Caron <maxime.deflou-caron1@etu.univ-lorraine.fr>
2026-09-18 09:38:55 +0200

    Ajout du diagramme de classe ainsi que des classes Etudiant et Identite

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,2 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Etudiant {
+    private Identite identite;
+    private Formation formation;
+    private HashMap<String, ArrayList<Double>> resultat;
+
+    public Etudiant(Identite id, Formation f){
+        this.identite = id;
+        this.formation = f;
+        this.resultat = new HashMap<>();
+    }
+
+    public void ajoutNote(String matiere, double note){
+        if (note < 0) note = 0;
+        if (note > 20) note = 20;
+        if (!this.resultat.containsKey(matiere)) this.resultat.put(matiere, new ArrayList<>());
+        this.resultat.get(matiere).add(note);
+    }
+
+    public double calculerMoyenne(String matiere){
+        if (!this.resultat.containsKey(matiere)) return 0;
+        double moyenne = 0;
+        for (double note : this.resultat.get(matiere)){
+            moyenne += note;
+        }
+        return moyenne / this.resultat.get(matiere).size();
+    }
+
+    public double calculerMoyenneGenerale(){
+        double mg = 0;
+        double mm = 0;
+        int cpte = 0;
+        for (String m : this.resultat.keySet()){
+            mm = calculerMoyenne(m);
+            mg += mm * this.formation.getCoeff(m);
+            cpte += this.formation.getCoeff(m);
+        }
+        return mg/cpte;
+    }
 }
--- src/Identite.java
+++ src/Identite.java
@@ -1,2 +1,20 @@
 public class Identite {
+    private String nip, nom, prenom;
+
+    public Identite(String ni, String n, String p){
+        this.nip = ni;
+        this.nom = n;
+        this.prenom = p;
+    }
+
+    public String getNIP(){
+        return this.nip;
+    }
+    public String getNom(){
+        return this.nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
 }
